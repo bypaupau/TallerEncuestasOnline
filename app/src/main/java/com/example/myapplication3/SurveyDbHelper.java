7bd012c7daa3c4dc5b2a64438c480e9634c17193
pau <145233797+bypaupau@users.noreply.github.com>
2026-07-07 16:51:15 -0500

    agregacion de preguntas

--- app/src/main/java/com/example/myapplication3/SurveyDbHelper.java
+++ app/src/main/java/com/example/myapplication3/SurveyDbHelper.java
@@ -19,19 +19,52 @@ public class SurveyDbHelper extends SQLiteOpenHelper {
         // se insertan las preguntas fijas, el usuario no las escribe
         db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
                 " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
-                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (1, '¿Cómo califica la atención recibida?')");
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (1, '¿Cómo calificarías la atención de secretaría general?')");
 
         db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
                 " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
-                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (2, '¿Recomendaría nuestro servicio?')");
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (2, '¿Cómo calificaría nuestra selección de docentes?')");
 
         db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
                 " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
-                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (3, '¿El personal fue amable?')");
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (3, '¿Considera el personal de mantenimiento efectivo y amable?')");
 
         db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
                 " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
                 SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (4, '¿La institución cumplió sus expectativas?')");
+
+        db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
+                " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (5, '¿Cómo calificaría la infraestructura de la institución?')");
+
+        db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
+                " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (6, '¿Cómo calificaría el Internet de la institución?')");
+
+        db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
+                " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (7, '¿Cómo calificaría el cuidado dado a las instalaciones?')");
+
+        db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
+                " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (8, '¿Qué tan fácil es usar la plataforma virtual de la institución?')");
+
+        db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
+                " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (9, '¿Cómo calificaría el servicio de cafetería?')");
+
+        db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
+                " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (10, '¿Está conforme con los horarios de clase asignados?')");
+
+        db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
+                " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (11, '¿Recibe la información importante a tiempo por parte de la institución?')");
+
+        db.execSQL("INSERT INTO " + SurveyContract.PreguntasEntry.TABLE_NAME +
+                " (" + SurveyContract.PreguntasEntry.COLUMN_ID_PREG + ", " +
+                SurveyContract.PreguntasEntry.COLUMN_TEXTO + ") VALUES (12, '¿Recomendaría esta institución a otra persona?')");
+
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
